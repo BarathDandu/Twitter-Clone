66ba7b1a797d6f5f2e265a94335d592a3e54f9b4
DESKTOP-6Q1MSRO\Barath <barathdandu01@gmail.com>
2021-06-24 23:22:59 +0530

    completed logout popover and half done with tweet modal, need to make changes to pictures so that they always appear in 16:9 ratio

--- home.php
+++ home.php
@@ -1,4 +1,18 @@
+<?php
 
+   if($_GET['name']=='lauda'){
+       
+        unset($_SESSION);
+
+        setcookie("email", "", time() - 60 * 60);
+
+        $_COOKIE['email'] = "";
+       
+        header("Location: http://barathdandu-com.stackstaging.com/p/welcome.php");  
+        exit();
+   }
+
+?>
 
 <!doctype html>
 <html lang="en">
@@ -38,9 +52,10 @@
           .texx{
             font-family: 'Work Sans', sans-serif;    
             font-weight: 900;
-            font-size: 0.9vw;
+            font-size: 0.8vw;
             color: #fff;
-          }       
+          }    
+       
            
           .col-4{
                 border: 1px solid #cccccc;
@@ -94,7 +109,6 @@
           }
           
           #cont{
-              border: 1px solid white;
               display: inline-block;
               width: auto; 
               margin-right: 3vh;
@@ -114,7 +128,7 @@
           #tweet-left-col{
               background-color:#1DA1F2; 
                border-radius: 40px;
-              padding: 1vh 9vh ;              
+              padding: 1.2vh 9vh ;              
               margin-top: 1.5vh;
               display: inline-block;
               clear:both;
@@ -140,6 +154,13 @@
           .profilepicleft{
               height: 4vh;
               margin: 1vh;  
+              border-radius: 50%;
+              float: left;
+          }
+          .profilepictweet{
+              width: 100%;
+              margin: 0;  
+              padding: 0;
               border-radius: 50%;
               float: left;
           }
@@ -178,8 +199,117 @@
              
           }
           
+          .popover {
+                background: #000;
+               box-shadow: 0 0 3px #fff;
+              z-index: 99;
+              color: #fff;
+            }
+       .bs-popover-top .arrow::after,
+        .bs-popover-auto[x-placement^="top"] .arrow::after {
+            border-top-color: #000; 
+            -webkit-filter: drop-shadow(0 2px 1px #666666);
+            filter: drop-shadow(0 2px 1px #666666);
             
-      
+        }
+          .logoutpopover{
+              background-color:rgba(29, 161, 242, .5); 
+               border-radius: 40px;
+              padding: 1vh 4vh ;              
+              margin: 1.5vh 1.5vh 2vh 1.5vh;
+              display: inline-block;
+              clear:both;
+              float:left;
+              color: #fff;
+          }
+          
+          .logoutpopover:hover{
+               background-color:rgba(29, 161, 242, 1); 
+              color: #fff;
+          }
+          
+          #tweetmodal{
+                background-color:rgba(36,45,52, 0.75); 
+                
+          }
+          .modal-backdrop.show {
+                opacity: 0;
+            }
+          .modal-body{
+                background-color: #000;
+              padding: 1vh 1vh 0 1vh ;
+            }
+          .modal-footer{
+                background-color: #000;
+                border-top-color: #2F3336;
+            }
+
+          .modal-header{
+                background-color: #000;
+                border-bottom-color: #2F3336;
+              padding: 0;
+          }
+        .tweetmodal{
+              background-color:#1DA1F2; 
+               border-radius: 40px;
+              padding: 1vh 2vh ;              
+              margin: 0vh;
+              display: inline-block;
+              float:left;
+              color: #fff;
+            float: right;
+          }
+          .modal-lg {
+              margin-top: 5.5vh;
+                max-width: 33%;
+              height: auto;
+            }
+              .modalclose {
+              position: relative;
+              background-color:  #000;
+                  color: #1DA1F2;
+            font-size: 200%;
+                  margin:0 0 0.75vh 1vh;       
+          }
+          
+          .modal-content {
+              border-radius: 15px !important;
+              border: 6px solid black!important;
+            }
+          
+          .textarea{
+                display: block;
+              margin:0.5vh 1vh;
+              width: 98%;
+              height: 12vh;
+              resize: both;
+              min-height: 40px;
+              line-height: 20px;
+              border:none;
+              resize: none;
+              color: #fff;
+              outline: none;
+            
+            
+          }
+          #mediaicon{
+               height: 2vh;
+              margin: 0; 
+              margin-right: 1.2vh;
+              float: left;
+          }
+          .modalfooter>input {
+              display: none;
+            }
+
+          #mediainserted{
+              max-width:85%;
+              max-height: 48%;
+              margin: 1vh;
+              border-radius: 20px;
+              float:right;
+            }
+          
       </style>
       
       
@@ -241,9 +371,10 @@
                     More
                 </a>
                 <br><br>
-                <input type = "button"  id = "tweet-left-col" class = "texx" value = "Tweet" >
+                <input type = "button"  id = "tweet-left-col" class = "texx" value = "Tweet" data-toggle="modal" data-target="#tweetmodal" >
         
-                  <a class = "foote" href="#" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover">      
+                  <a class = "foote popover-dismiss" data-html='true' tabindex="0" data-toggle="popover" data-placement="top"  
+                     data-content="<a class = 'logoutpopover texx' href='http://barathdandu-com.stackstaging.com/p/home.php?name=lauda'>Log Out</a>.">      
                       <img class = "profilepicleft" src="/p/images/userIcon.png" > 
                       <div class = "data">
                        <span class="name"> Barath Dandu</span> 
@@ -253,7 +384,47 @@
                           <img class = "treedots hovlogo" src="/p/images/tdh.png" >   
                 </a>
             </div>
+            
+                
+            <!-- Modal -->
+            <div class="modal fade" id="tweetmodal" tabindex="-1" >
+              <div class="modal-dialog modal-lg">
+                <div class="modal-content   ">
+                  <div class="modal-header">
+                    <button type="button" class="modalclose" data-dismiss="modal">
+                      <span >&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body" >
+                     
+                      <div style = "height:100%; width:10%; float:left;">
+                            <img class = "profilepictweet" src="/p/images/userIcon.png" > 
+                      </div>
+                      <div id = "te" style = "float:left; width:90%;height:auto;">
+                         <span class="textarea" role="textbox" contenteditable placeholder="Remember, be nice!"></span>
+                      </div>
+                      
+                          <img id="mediainserted" src=""  >
+                        
+                    
+                      <div class = "modalfooter" style = "float:right; width:90%;height:auto;margin:0; ">
+                            
+                            <label for = "fileinp" class = "logodiv" id = "insrtpht">  
+                                <img id = "mediaicon" src="http://barathdandu-com.stackstaging.com/p/images/insertmedia.png" alt="Twitter">
+                            </label>
+                            <input id="fileinp" type="file" accept="image/png, image/gif, image/jpeg" >
+
+                            <button type="button" class="tweetmodal texx">Tweet</button>
+                      </div>
+                  </div>
+                 
+                </div>
+              </div>
             </div>
+              
+              
+              
+              </div>
             <div class="col-5">col-5</div>
             <div class="col-4">col-4</div>
             </div>
@@ -262,11 +433,14 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 
-    <script type="text/javascript">
-        
-        $("#tweet-left-col").click(function(e){
-            alert("hello");
-        })
+    <script >
+        fileinp.onchange = evt => {
+          const [file] = fileinp.files
+          if (file) {
+            mediainserted.src = URL.createObjectURL(file)
+          }
+        }
+
      
         $(".left-col").hover(function(){
            $(this).children('img:nth(0)').hide();
@@ -281,17 +455,26 @@
            $(this).children('img:nth(2)').show();
         }, function(){
             $(this).children('img:nth(1)').show();
-           $(this).children('img:nth(2)').hide();        
+           $(this).children('img:nth(2)').hide();     
         });
       
         $(function () {
-          $('[data-toggle="popover"]').popover()
-        })
-      
-      
-      
-      
-      
+          $('[data-toggle="popover"]').popover({html:true,sanitize: false})
+        });
+        
+        $('#te').on('input', function () {
+            this.style.height = 'auto';
+             
+            this.style.height =
+                    (this.scrollHeight) + 'px';
+        });
+     
+
+        $(document).on("click", "#funcid", function () {
+            $(this).parents(".popover").popover('hide');
+        });
+
+
     </script>
   </body>
 </html>
